3991ea06e07f93e3c5e7f1ea48579fc7b29f540f
Imamkoo <imammm.kooo@gmail.com>
2021-12-14 14:36:39 +0700

    Tugas 8 javascript

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function mobilephone() {
+    let phone = {
+        merk: 'Apple',
+        harga: 8000000,
+        warna: 'silver',
+        type: ['ip8plus', 'ip7plus', 'ip11', 'ip12pro']
+    }
+    phone.harga = 7500000
+    console.log(phone)
+}
+
+mobilephone()
